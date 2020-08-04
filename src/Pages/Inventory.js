58dc098006fbdc28b7525d47e6967ccbb2afdcb8
George Torres <torresgeorge2014@gmail.com>
2020-08-03 20:25:12 -0500

    Fixed bug with backend connection. Needed to add http:// to filepath. Done while mob programming with entire team.

--- src/Pages/Inventory.js
+++ src/Pages/Inventory.js
@@ -30,7 +30,7 @@ const Inventory = (props) => {
     };
 
     axios
-      .post('Capstone3-env-1.eba-zhpmpeyi.us-east-2.elasticbeanstalk.com/products', params)
+      .post('http://Capstone3-env-1.eba-zhpmpeyi.us-east-2.elasticbeanstalk.com/products', params)
       .then((res) => {
         console.log(res);
         alert('Product added successfully');
