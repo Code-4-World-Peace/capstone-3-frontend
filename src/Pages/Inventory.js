b1fab1a496ea581b55a0c40453b4a36aab240846
SoraVladd <julianrladd@gmail.com>
2020-08-06 12:45:52 -0500

    Worked with the whole team to fix add product function

--- src/Pages/Inventory.js
+++ src/Pages/Inventory.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import ProductAddView from '../Components/ProductAddView';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ProductAddView from "../Components/ProductAddView";
 
 const Inventory = (props) => {
   const [products, setProducts] = useState([]);
@@ -17,7 +17,7 @@ const Inventory = (props) => {
     };
 
     axios
-      .post('http://localhost:8080/products', params)
+      .post("http://localhost:8080/products", params)
       .then(function (response) {
         console.log(response);
       })
@@ -28,7 +28,7 @@ const Inventory = (props) => {
 
   const getProducts = () => {
     axios
-      .get('http://localhost:8080/products')
+      .get("http://localhost:8080/products")
       .then((res) => {
         setProducts(res.data);
       })
@@ -59,39 +59,57 @@ const Inventory = (props) => {
       ) : null}
       <br />
       <br />
-      <div className='productContainer'>
-        <div id='invalidSearch' style={{ display: 'none' }}>
+      <div className="productContainer">
+        <div id="invalidSearch" style={{ display: "none" }}>
           No Search results found!
         </div>
         {products.map((product, i) => {
           // Map thru all the products and create a card for each of them
           return (
-            <div className='card' key={products[i].sku} id={products[i].sku}>
-              <h5 className='card-header'>{products[i].name}</h5>
+            <div className="card" key={products[i].sku} id={products[i].sku}>
+              <h5 className="card-header">{products[i].name}</h5>
               <img
                 src={products[i].image}
-                className='card-img-top'
+                className="card-img-top"
                 alt={products[i].name}
-                style={{ maxWidth: '200px', margin: 'auto' }}
+                style={{ maxWidth: "200px", margin: "auto" }}
               />
-              <div className='card-body'>
-                <h5 className='card-title'>${products[i].price}</h5>
-                <p className='card-text'>
+              <div className="card-body">
+                <h5 className="card-title">${products[i].price}</h5>
+                <p className="card-text">
                   {products[i].description}
                   <br />
                   {products[i].heat}
                   <br />
                   {products[i].flavor}
                 </p>
-                <p className='card-text'>Current Stock: {products[i].stock}</p>
-                <input className='card-text' placeholder="Input new stock quantity" onChange={(e) => setInput(e.target.value)} />
-                <p className='card-text'>
+                <p className="card-text">Current Stock: {products[i].stock}</p>
+                <input
+                  className="card-text"
+                  onChange={(e) => {
+                    setInput(e.target.value);
+                  }}
+                  type="number"
+                  min="0"
+                  max="500"
+                />
+                <p className="card-text">
                   <small>SKU: 00{products[i].sku}</small>
                 </p>
-                <button className='btn btn-primary' onClick={() => { updateProduct(products[i].name, input) }}>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => {
+                    if (Number(input) >= 0) {
+                      updateProduct(
+                        products[i].name,
+                        Number(input) + Number(products[i].stock)
+                      );
+                    }
+                  }}
+                >
                   Update Product
                 </button>
-                <p className='card-text'>
+                <p className="card-text">
                   <small>Tags: {products[i].tag} </small>
                 </p>
               </div>
