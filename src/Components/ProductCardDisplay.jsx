61ac28749bff95f277965dfab8b4ba41a5d6636b
SoraVladd <julianrladd@gmail.com>
2020-08-05 20:01:00 -0500

    ver 1.3 of working on ProductCardDisplay with Zach,Nancy,George,Tyler and Julian.

--- src/Components/ProductCardDisplay.jsx
+++ src/Components/ProductCardDisplay.jsx
@@ -3,84 +3,84 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 const ProductCardDisplay = (props) => {
-  const { stock, products, addToCart, setShow, setToastBody } = props;
-  const [displayBody] = useState([]);
+    const { stock, products, addToCart, setShow, setToastBody } = props;
+    const [displayBody] = useState([]);
 
-  //   const setDisplayProduct = () => {
-  //     products.map((product) => {
-  //       console.log(product);
-  //       displayBody.push(
-  //         <>
-  //           <h5 className='card-header'>{product.name}</h5>
-  //           <p className='card-text'>Stock: {product.stock}</p>
-  //           <p className='card-text'>
-  //             <small>SKU: 00{product.sku}</small>
-  //           </p>
-  //         </>
-  //       );
+    //   const setDisplayProduct = () => {
+    //     products.map((product) => {
+    //       console.log(product);
+    //       displayBody.push(
+    //         <>
+    //           <h5 className='card-header'>{product.name}</h5>
+    //           <p className='card-text'>Stock: {product.stock}</p>
+    //           <p className='card-text'>
+    //             <small>SKU: 00{product.sku}</small>
+    //           </p>
+    //         </>
+    //       );
 
-  //       return '';
-  //     });
-  //   };
+    //       return '';
+    //     });
+    //   };
 
-  // useEffect(() => {
-  //   setDisplayProduct();
-  // }, []);
+    // useEffect(() => {
+    //   setDisplayProduct();
+    // }, []);
 
-  const displayProduct = () => {
-    products.map((product) => {
-      console.log(product);
-      return (
-        <div className='card' key={product.sku} id={product.sku}>
-          {product.map((productProp) => {
-            console.log(productProp)
-            switch (productProp) {
-              case "name":
-                console.log(productProp);
-                console.log(productProp.key);
-                <h5 className='card-header'>{product.name}</h5>;
-                break;
-              case "stock":
-                <p className='card-text'>Stock: {product.stock}</p>;
-                break;
-              case "sku":
-                <p className='card-text'>
-                  <small>SKU: 00{product.sku}</small>
-                </p>;
-                break;
-              default:
-                break;
-            }
-          })}
-          <button
-            className='btn btn-primary'
-            onClick={() => {
-              if (product.stock > 0) {
-                // Won't let user add item to cart if no stock is left
-                addToCart(i);
-                setToastBody(product.name);
-                setShow(true);
-              }
-            }}
-          >
-            Add to Cart
-          </button>
+    const displayProduct = () => {
+        return products.map((product) => {
+            console.log(product);
+            return (
+                <div className='card' key={product.sku} id={product.sku}>
+                    {Object.keys(product).map((productProp) => {
+                        console.log(productProp)
+                        switch (productProp) {
+                            case "name":
+                                console.log(productProp);
+                                console.log(productProp.name);
+                                return <h5 className='card-header'>{product.name}</h5>;
+
+                            case "stock":
+                                return <p className='card-text'>Stock: {product.stock}</p>;
+
+                            case "sku":
+                                return <p className='card-text'>
+                                    <small>SKU: 00{product.sku}</small>
+                                </p>;
+
+                            default:
+                                return '';
+                        }
+                    }).join()}
+                    <button
+                        className='btn btn-primary'
+                        onClick={() => {
+                            if (product.stock > 0) {
+                                // Won't let user add item to cart if no stock is left
+                                addToCart(product);
+                                setToastBody(product.name);
+                                setShow(true);
+                            }
+                        }}
+                    >
+                        Add to Cart
+                    </button>
+                </div>
+            );
+        }).join();
+    };
+
+    //   setDisplayProduct();
+
+    return (
+        <div className='productContainer'>
+            <div id='invalidSearch' style={{ display: 'none' }}>
+                No Search results found!
         </div>
-      );
-    });
-  };
 
-  //   setDisplayProduct();
-
-  return (
-    <div className='productContainer'>
-      <div id='invalidSearch' style={{ display: 'none' }}>
-        No Search results found!
-      </div>
-
-      {displayProduct()}
-    </div>
-  );
+            {displayProduct()}
+        </div>
+    );
 };
 
 export default ProductCardDisplay;
